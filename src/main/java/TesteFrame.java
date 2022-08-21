46ed8bfa29622ee5e783311158ce6b4d6a66c6a4
Rodney Lucas V. Oliveira <rodneylucas.oliveira@gmail.com>
2022-08-21 00:01:03 -0300

    Commit para guardar exercícios

--- src/main/java/TesteFrame.java
+++ src/main/java/TesteFrame.java
@@ -1,7 +1,12 @@
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.devtools.v102.page.model.Frame;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.netty.handler.codec.http.DefaultHttpContent;
 
 public class TesteFrame {
 private WebDriver driver;
@@ -17,7 +22,13 @@ private WebDriver driver;
 	
 	@Test
 	public void DeveInteragirComFrame ( ) {
-		
+		driver.switchTo().frame("frame1");
+		driver.findElement(By.id("frameButton")).click();
+		Alert alerta = driver.switchTo().alert();
+		String alert_text = alerta.getText();
+		alerta.accept();
+		driver.switchTo().defaultContent();
+		driver.findElement(By.id("elementosForm:nome")).sendKeys(alert_text);
 	}
 
 }
